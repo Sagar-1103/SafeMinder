5d0aaeb5552bcb516518ee94f5bba331d97f21fb
Sagar Shirgaonkar <sagarshirgaonkar59511@gmail.com>
2025-01-20 16:09:42 +0530

    firebase setup done

--- App.jsx
+++ App.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import {LogBox, Text, View} from 'react-native';
+import {LogBox, Text, TouchableOpacity, View} from 'react-native';
 LogBox.ignoreLogs(['new NativeEventEmitter']);
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const Tab = createBottomTabNavigator();
 
 const App = () => {
+
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
-    </Tab.Navigator>
+    <View>
+      <TouchableOpacity>
+        <Text>Hello</Text>
+      </TouchableOpacity>
+    </View>
   );
 };
 
