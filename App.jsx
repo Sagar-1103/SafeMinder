dfd7434a7b0b6bd7093a853d24bc01c2525c6fc7
Saksham-T <saksham.thakur0216@gmail.com>
2025-01-21 20:55:38 +0530

    RoleScreen UI Improved

--- App.jsx
+++ App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import {LogBox} from 'react-native';
 import FitComponent from './components/Fit';
 import SplashScreen from './screens/commonScreens/SplashScreen';
+import RoleScreen from './screens/commonScreens/RoleScreen';
 LogBox.ignoreLogs(['new NativeEventEmitter']);
 
 
 const App = () => {
 
   return (
-   <FitComponent/>
+   <RoleScreen/>
   );
 };
 
