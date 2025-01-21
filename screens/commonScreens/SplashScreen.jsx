89f857b775c715e1fc479f0d7de915789fe051d2
ved_shetye <vedbshetye03@gmail.com>
2025-01-21 17:16:07 +0530

    added common screens

--- screens/commonScreens/SplashScreen.jsx
+++ screens/commonScreens/SplashScreen.jsx
@@ -1,12 +1,37 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { View, Image, StyleSheet, Text } from 'react-native';
 
 const SplashScreen = () => {
   return (
-    <View>
-      <Text>SplashScreen</Text>
-    </View> 
-  )
-}
+    <View style={styles.container}>
+      <Image
+        source={require('../../assets/safeMinder_logo.jpg')} 
+        style={styles.logo}
+      />
+      <Text style={styles.text}>safeMinder</Text>
+    </View>
+  );
+};
 
-export default SplashScreen
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height:'100%',
+    width:'100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5F5F5',
+  },
+  logo: {
+    width: '44.8%',
+    height: '20%',
+  },
+  text: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    color: '#333',
+    marginTop: 20,
+  },
+});
+
+export default SplashScreen;
