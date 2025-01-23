3366da0abaded1e9f52a3bd4185f62fd12dbedac
ved_shetye <vedbshetye03@gmail.com>
2025-01-24 00:40:18 +0530

    added pie chart in reports

--- components/StepsBlock.jsx
+++ components/StepsBlock.jsx
@@ -1,25 +1,32 @@
 import React from 'react';
 import { View, Text, StyleSheet, Image, Dimensions } from 'react-native';
-import PieChart from 'react-native-pie-chart'
+import PieChart from 'react-native-pie-chart';
+
+const StepsBlock = ({ steps, distance, calories, goals }) => {
 
 const series = [
-  {value: 4000, color: rgb(229, 86, 25)},
-  {value: 2000, color: rgb(239, 170, 141)}
-]
-
-const StepsBlock = ({ steps, distance, calories }) => {
+  { value: steps, color: '#F42' },
+  { value: goals - steps, color: '#FAB' },
+];
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Steps</Text>
       <View style={styles.innerContainer}>
-        <PieChart widthAndHeight={'45%'} series={series} cover={0.45} />
+        <View style={styles.piechartContainer}>
+          <PieChart widthAndHeight={140} series={series} cover={0.75} style={styles.piechart} />
+          {/* Image and Text Overlay */}
+          <View style={styles.overlayContent}>
+            <Image source={require('../assets/sms.png')} style={styles.image} />
+            <Text style={styles.centerText}>{`${steps}`}</Text>
+          </View>
+        </View>
         <View>
           <Text style={styles.detail}>Number of Steps: </Text>
           <Text style={styles.detailValue}>{steps || '0'}</Text>
           <Text style={styles.detail}>Distance: </Text>
-          <Text style={styles.detailValue}>{distance || '0 km'}</Text>
+          <Text style={styles.detailValue}>{distance || '0'} km</Text>
           <Text style={styles.detail}>Calories Burned: </Text>
-          <Text style={styles.detailValue}>{calories || '0 kcal'}</Text>
+          <Text style={styles.detailValue}>{calories || '0'} kcal</Text>
         </View>
       </View>
     </View>
@@ -31,39 +38,58 @@ const { width } = Dimensions.get('window');
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#fff',
-    padding: '5%', // Padding as a percentage of the screen
+    padding: '5%',
     paddingBottom: '0%',
     borderRadius: 12,
     borderWidth: 0.5,
     elevation: 20,
     marginBottom: '4%',
-    width: '90%', // Width relative to the screen size
-    alignSelf: 'center', // Center the block
+    width: '90%',
+    alignSelf: 'center',
   },
   title: {
     fontSize: width * 0.05,
     fontWeight: 'bold',
     marginBottom: '7%',
-    color: '#000000'
+    color: '#000000',
   },
   innerContainer: {
     flexDirection: 'row',
     alignItems: 'center',
     marginBottom: '9%',
   },
-  icon: {
-    width: '45%', // Icon width relative to parent container
-    height: undefined, // Maintain aspect ratio
-    aspectRatio: 1, // Force square aspect ratio
-    marginRight: '4%',
+  piechartContainer: {
+    position: 'relative',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  piechart: {
+    marginRight: '5%',
+  },
+  overlayContent: {
+    position: 'absolute',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: '50%', // Adjust the size of the image
+    height: '100%',
+    marginRight: '15%'
+    // borderRadius: 15, // For circular image
+    // marginBottom: 5,
+  },
+  centerText: {
+    fontSize: width * 0.06,
+    fontWeight: 'bold',
+    marginRight: '15%',
+    color: '#000',
   },
   detail: {
-    fontSize: width * 0.035, // Font size as a percentage of screen width
+    fontSize: width * 0.035,
     color: '#666',
-    // marginBottom: '1%',
   },
   detailValue: {
-    fontSize: width * 0.045, // Font size as a percentage of screen width
+    fontSize: width * 0.045,
     color: 'rgb(229, 86, 25)',
     fontWeight: '600',
     marginBottom: '6%',
